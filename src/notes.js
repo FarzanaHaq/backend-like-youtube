b604da9d89b0cd09a3d357bde364a0f0c9511dfa
FarzanaHaq <riditafarzana@gmail.com>
2024-07-28 20:10:10 +0600

    mongo connected

--- src/notes.js
+++ src/notes.js
@@ -1,8 +1,27 @@
 /*
 
-6: npm init, git add, 
-nodemon: server start kore. dev dependency production e kaje lage na.
+6.
 
+npm init, git add, 
+nodemon: server start kore. 
+dev dependency production e kaje lage na.
+prettier install
+
+7.
+
+riditafarzana
+IQaQY0CCkFfgitXW
+
+
+mongodb+srv://riditafarzana:<password>@cluster0.kzjdovx.mongodb.net/
+
+
+
+
+try...catch works like this:
+try Block: This is where you put the code that might have problems. It's like making your sandwich. You're hoping everything goes well.
+
+catch Block: This is where you handle the problems if they happen. If you run out of bread, you can handle it by maybe using a different type of bread or making a different snack.
 
 
 
